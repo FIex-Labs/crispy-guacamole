744e98f2630c94b43d7c0c849e7d4153e9dbdc52
Alvin Zhang <alvin.zhang295@gmail.com>
2022-11-20 20:38:31 -0600

    Init React App

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import NavBar from "./Components/NavigationBar";
+
+function App() {
+  return (
+    <div className="background">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Components/NavigationBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <header style={{ position: "sticky", top: "0px" }}>
+      <p>Hi</p>
+    </header>
+  );
+}
