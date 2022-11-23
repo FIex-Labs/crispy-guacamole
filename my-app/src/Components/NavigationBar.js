3483ad4993260f65b704e19a957fd5919e5253d5
Alvin Zhang <alvin.zhang295@gmail.com>
2022-11-22 18:06:52 -0600

    Nav Bar and styling
    Temporary content

--- my-app/src/Components/NavigationBar.js
+++ my-app/src/Components/NavigationBar.js
@@ -1,9 +1,28 @@
 import React from "react";
+import "../Styles/NavBar.css";
 
 export default function NavBar() {
   return (
-    <header style={{ position: "sticky", top: "0px" }}>
-      <p>Hi</p>
-    </header>
+    // <ul>
+    //   <li>
+    //     <button className="header_button">Pricing</button>
+    //   </li>
+    //   <li>
+    //     <button className="header_button">Contact</button>
+    //   </li>
+    // </ul>
+    <ul>
+      <li>
+        <a href="#home">Home</a>
+      </li>
+      <li>
+        <a href="#contact">Contact</a>
+      </li>
+      <li>
+        <a class="active" href="#about">
+          About
+        </a>
+      </li>
+    </ul>
   );
 }
