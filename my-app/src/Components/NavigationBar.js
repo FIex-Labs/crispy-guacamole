6bf17e150654989e76977404bb3151995141b225
Alvin Zhang <alvin.zhang295@gmail.com>
2022-11-25 00:20:48 -0600

    Routing implemented
    Add links to other pages from navigation bar

--- my-app/src/Components/NavigationBar.js
+++ my-app/src/Components/NavigationBar.js
@@ -1,21 +1,32 @@
 import React from "react";
 import "../Styles/NavBar.css";
 import FlexLabsPng from "../Media/flexlabs.png";
-
+import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <ul>
-      <li>
-        <img style={{ width: "45px" }} src={FlexLabsPng} alt="Logo" />
+      <li style={{ padding: "0px" }}>
+        <Link to="/pricing" preventScrollReset={true}>
+          <img style={{ width: "45px" }} src={FlexLabsPng} alt="Logo" />
+        </Link>
       </li>
       <li>
-        <a href="#home">Pricing</a>
+        <Link className="text" to="/pricing" preventScrollReset={true}>
+          {" "}
+          Pricing
+        </Link>
       </li>
       <li>
-        <a href="#contact">Contact</a>
+        <Link className="text" to="/contact" preventScrollReset={true}>
+          {" "}
+          Contact
+        </Link>
       </li>
       <li>
-        <a href="#about">Use Cases</a>
+        <Link className="text" to="/usage" preventScrollReset={true}>
+          {" "}
+          Use Cases
+        </Link>
       </li>
     </ul>
   );
