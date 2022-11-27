bf7314c222080e69ef77f51ffe494946e3e9d70c
Alvin Zhang <alvin.zhang295@gmail.com>
2022-11-27 17:34:48 -0600

    Add sections
    Sections related to product now inside of app

--- my-app/src/Pages/App.js
+++ my-app/src/Pages/App.js
@@ -1,6 +1,10 @@
+import { useEffect } from "react";
 import "../Styles/App.css";
 import LandingPage from "../Sections/LandingSection";
-import { useEffect } from "react";
+import NaturalLanguage from "../Sections/NaturalLanguage";
+import AutomaticProvisioning from "../Sections/AutomaticProvisioning";
+import UseWithExisting from "../Sections/UseWithExisting";
+import Footer from "../Sections/Footer";
 
 export default function App() {
   const observer = new IntersectionObserver((entries) => {
@@ -8,7 +12,6 @@ export default function App() {
       if (entry.isIntersecting) {
         entry.target.classList.add("show");
       } else {
-        entry.target.classList.remove("show");
       }
     });
   });
@@ -23,53 +26,9 @@ export default function App() {
   return (
     <div>
       <LandingPage />
-      <section>
-        <h1> Easy to use </h1>
-        <p>
-          {" "}
-          Tell us in plain english what you would like to store. We'll help you
-          decide what data model to use, and generate schemas for you
-          automatically.{" "}
-        </p>
-      </section>
-      <section>
-        <h1> Automatic provisioning</h1>
-        <p>
-          {" "}
-          Confused about all the configuration options when hosting a database
-          in the cloud? We take note of your preferences as well as the database
-          needs, and come up with the best configurations for you.
-        </p>
-      </section>
-      <section>
-        <h1> Use with existing database</h1>
-        <p>
-          If you have an existing database, it is easy to migrate your database
-          and use our platform. We provide a suite of tools to help your team
-          work with the database in an easy manner.
-        </p>
-        <div className="subsection">
-          <div>
-            <h3>Human readable queries</h3>
-            <p>
-              {" "}
-              Use natural language to generate complex queries to your database{" "}
-            </p>
-          </div>
-          <div>
-            <h3>Extend your database</h3>
-            <p>
-              {" "}
-              Add tables and connections to other databases, regardless of the
-              data model
-            </p>
-          </div>
-          <div>
-            <h3>Useful insights</h3>
-            <p>Generate summary statistics and visuals </p>
-          </div>
-        </div>
-      </section>
+      <NaturalLanguage />
+      <AutomaticProvisioning />
+      <UseWithExisting />
     </div>
   );
 }
