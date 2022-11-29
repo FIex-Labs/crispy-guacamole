b4eccdb81f8246a7fcd6139446a9cbec8bf22d5b
Alvin Zhang <alvin.zhang295@gmail.com>
2022-11-28 22:58:09 -0600

    Enable CSS styling from wrapper
    Allow style prop, make width 100%

--- my-app/src/Components/NavigationBar.js
+++ my-app/src/Components/NavigationBar.js
@@ -2,32 +2,34 @@ import React from "react";
 import "../Styles/NavBar.css";
 import FlexLabsPng from "../Media/flexlabs.png";
 import { Link } from "react-router-dom";
-export default function NavBar() {
+export default function NavBar(props) {
   return (
-    <ul>
-      <li style={{ padding: "0px" }}>
-        <Link to="/pricing">
-          <img style={{ width: "45px" }} src={FlexLabsPng} alt="Logo" />
-        </Link>
-      </li>
-      <li>
-        <Link className="text" to="/pricing">
-          {" "}
-          Pricing
-        </Link>
-      </li>
-      <li>
-        <Link className="text" to="/contact">
-          {" "}
-          Contact
-        </Link>
-      </li>
-      <li>
-        <Link className="text" to="/usage">
-          {" "}
-          Use Cases
-        </Link>
-      </li>
-    </ul>
+    <div style={props.style}>
+      <ul>
+        <li style={{ padding: "0px" }}>
+          <Link to="/pricing">
+            <img style={{ width: "45px" }} src={FlexLabsPng} alt="Logo" />
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to="/pricing">
+            {" "}
+            Pricing
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to="/contact">
+            {" "}
+            Contact
+          </Link>
+        </li>
+        <li>
+          <Link className="text" to="/usage">
+            {" "}
+            Use Cases
+          </Link>
+        </li>
+      </ul>
+    </div>
   );
 }
