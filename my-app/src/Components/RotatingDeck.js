d3c5a5ff872a7632a09ffde58899ee78f793b6bb
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-01 23:38:28 -0600

    Update RotatingDeck.js
    Remove unused imports and allow style from top level

--- my-app/src/Components/RotatingDeck.js
+++ my-app/src/Components/RotatingDeck.js
@@ -1,30 +1,8 @@
 import "../Styles/RotatingDeck.css";
-import { useEffect } from "react";
+
 export default function RotatingDeck(props) {
-  const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      let Deck = document.getElementById("Deck");
-      console.log(Deck);
-
-      console.log(entry.target.id);
-      let child = document.getElementById(entry.target.id);
-
-      if (entry.isIntersecting) {
-        entry.target.classList.add("AnimateDeckFade");
-      } else {
-        entry.target.classList.add("AnimateDeckFade");
-      }
-    });
-  });
-
-  const hiddenElements = document.querySelectorAll(".AnimateDeckItem");
-  console.log();
-  hiddenElements.forEach((el) => {
-    observer.observe(el);
-  });
-
   return (
-    <div id="DeckHolder" style={{ width: "inherit" }}>
+    <div id="DeckHolder" style={props.style}>
       <ul
         id="Deck"
         className=""
@@ -39,7 +17,7 @@ export default function RotatingDeck(props) {
             <li
               id={i}
               className="AnimateDeckItem"
-              style={{ padding: "0px 25px 0px 25px", display: "block" }}
+              style={{ margin: "25px 25px 25px 25px", display: "block" }}
             >
               {img}
             </li>
