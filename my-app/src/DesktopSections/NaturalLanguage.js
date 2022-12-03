158073869ac5c97b4753764ce03994415089927b
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-03 13:25:03 -0600

    Page formatting change
    Images on left, text on right

--- my-app/src/DesktopSections/NaturalLanguage.js
+++ my-app/src/DesktopSections/NaturalLanguage.js
@@ -2,6 +2,7 @@ import "../Styles/ProductDetails.css";
 import RotatingDeck from "../Components/RotatingDeck";
 import RedisLogo from "../Media/Logos/RedisTextLogo.svg";
 import PostgresLogo from "../Media/Logos/PostgreSQLLogo.svg";
+import HorizontalSpacer from "../Components/HorizontalSpacer";
 
 import { Section1Header, Section1HeaderDesc } from "../Components/TextContent";
 export default function NaturalLanguage() {
@@ -14,43 +15,79 @@ export default function NaturalLanguage() {
         <p className="Section_Header_Description">
           <Section1HeaderDesc />
         </p>
-        <RotatingDeck>
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-          <img className="Deck" src={RedisLogo} alt="RedisLogo" />
-          <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
-        </RotatingDeck>
-        <div>
-          <div className="hidden Section">
-            <h1 className="Section_Text">Data models that fit</h1>{" "}
-            <p className="paragraph">
-              The choice of which database to use can be tough. We'll analyze
-              your use cases and find the best one for you.
-            </p>
+        <HorizontalSpacer />
+        <div className=" Section">
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "55% 45%",
+              alignItems: "center",
+            }}
+          >
+            <RotatingDeck>
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+            </RotatingDeck>
+
+            <div style={{ margin: "0px 0px 0px 25px" }}>
+              <h1 className="Section_Text">Find your database</h1>{" "}
+              <p className="paragraph">
+                The choice of which database to use can be tough. We'll analyze
+                your use cases and find the best one for you.
+              </p>
+            </div>
           </div>
-          <div className="hidden Section">
-            {" "}
-            <h1 className="Section_Text">
-              Intelligently generated schemas
-            </h1>{" "}
-            <p className="paragraph">
-              {" "}
-              Tell us about your data and what it looks like. Our product will
-              give you a head start by generating a schema that best fits your
-              data. You can modify the schema afterwards to fit your ideas.
-            </p>
+        </div>
+
+        <div className="Section">
+          <div
+            style={{
+              display: "grid",
+              gridTemplateColumns: "55% 45%",
+              alignItems: "center",
+            }}
+          >
+            <RotatingDeck>
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+              <img className="Deck" src={RedisLogo} alt="RedisLogo" />
+              <img className="Deck" src={PostgresLogo} alt="PostgresLogo" />
+            </RotatingDeck>
+            <div style={{ margin: "0px 0px 0px 25px" }}>
+              <h1 className="Section_Text">Intelligently generated schemas</h1>
+              <p className="paragraph">
+                {" "}
+                Tell us about your data and what it looks like. Our product will
+                give you a head start by generating a schema that best fits your
+                data. You can modify the schema afterwards to fit your ideas.
+              </p>
+            </div>
           </div>
         </div>
       </section>
