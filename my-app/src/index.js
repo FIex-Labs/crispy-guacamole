6a58d2a5b8ff6a6d9539577ddee077cc2dce222f
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-02 20:11:56 -0600

    Update to HashRouter
    BrowserRouter doesn't work on GH Pages

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,44 +1,42 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./Pages/App.js";
+
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import {
-  BrowserView,
-  MobileView,
-  isBrowser,
-  isMobile,
-} from "react-device-detect";
-import MobileApp from "./Pages/MobileApp";
+import { HashRouter, BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserView, MobileView } from "react-device-detect";
+
+// Import Pages
+import App from "./pages/App.js";
+import MobileApp from "./pages/MobileApp";
+import Pricing from "./pages/Pricing";
+import Contact from "./pages/Contact";
+import UseCases from "./pages/UseCases";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserView>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="/contact" element={<p>contact</p>} />
-          <Route path="/pricing" element={<p>pricing</p>} />
-          <Route path="/usage" element={<p>use cases</p>} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/pricing" element={<Pricing />} />
+
+          <Route exact path="/" element={<App />} />
+          <Route exact path="usage" element={<UseCases />} />
+          <Route path="/*" element={<p>hi</p>} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </BrowserView>
     <MobileView>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
-          <Route path="/" element={<MobileApp />} />
-          <Route path="/contact" element={<p>contact</p>} />
-          <Route path="/pricing" element={<p>pricing</p>} />
-          <Route path="/usage" element={<p>use cases</p>} />
+          <Route exact path="/" element={<MobileApp />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/pricing" element={<Pricing />} />
+          <Route exact path="/usage" element={<UseCases />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </MobileView>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
