118f595f8f0c360017c5c92d4e61540a1878d7d7
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-05 16:30:24 -0600

    Add chevron

--- my-app/src/Components/PillButton.js
+++ my-app/src/Components/PillButton.js
@@ -1,12 +1,21 @@
 import "../Styles/PillButton.css";
 import { Link } from "react-router-dom";
-
+import Chevron from "../Media/Chevron.svg";
 export default function PillButton(props) {
   return (
     <button className="pill" style={props.style}>
-      <Link style={{ textDecoration: "none" }} to={props.link}>
+      <Link
+        style={{ textDecoration: "none", color: "#000000" }}
+        to={props.link}
+      >
         {props.children}
       </Link>
+      <img
+        className="Chevron"
+        style={{ height: "75%", margin: "1px -5px 0px 10px" }}
+        src={Chevron}
+        alt="Chevron"
+      />
     </button>
   );
 }
