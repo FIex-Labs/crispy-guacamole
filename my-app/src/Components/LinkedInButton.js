8f3f23f91834da31de9c4e4a6f31bb419adff4df
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-05 16:29:43 -0600

    Update LinkedInButton.js
    OPen in inew page

--- my-app/src/Components/LinkedInButton.js
+++ my-app/src/Components/LinkedInButton.js
@@ -4,7 +4,7 @@ export default function LinkedInButton() {
   let linkedinpage = "https://www.linkedin.com/company/flex-labs";
   return (
     <div>
-      <a href={linkedinpage}>
+      <a href={linkedinpage} rel="noopener noreferrer" target="_blank">
         <img style={{ width: "10%" }} src={LinkedInImage} alt="LinkedIn Icon" />
       </a>
     </div>
