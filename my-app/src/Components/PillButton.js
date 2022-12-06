6d2381fc680092fc5d5ac27bf467c14fc6aa9f05
Alvin Zhang <alvin.zhang295@gmail.com>
2022-12-06 11:06:52 -0600

    Fix button redirect
    Button doesn't work if you click on chevron

--- my-app/src/Components/PillButton.js
+++ my-app/src/Components/PillButton.js
@@ -1,18 +1,41 @@
 import "../Styles/PillButton.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import Chevron from "../Media/Chevron.svg";
+import { useNavigate } from "react-router-dom";
+import { useCallback } from "react";
 export default function PillButton(props) {
+  const navigate = useNavigate();
+  let link = props.link;
+  const handleOnClick = useCallback(() => navigate(link));
+
+  let translate_dist = 3;
+  const moveChevron = (event) => {
+    let icon = event.target;
+    icon.children[0].style.transitionDuration = "0.25s";
+    // icon.style.width = `${width + translate_dist}px`;
+    icon.children[0].style.transform = `translateX(${translate_dist}px)`;
+  };
+  const moveBackChevron = (event) => {
+    let icon = event.target;
+    icon.children[0].style.transitionDuration = "0.25s";
+    // icon.style.width = `${width - translate_dist}px`;
+    icon.children[0].style.transform = `translateX(${-translate_dist}px)`;
+  };
+
   return (
-    <button className="pill" style={props.style}>
-      <Link
-        style={{ textDecoration: "none", color: "#000000" }}
-        to={props.link}
-      >
-        {props.children}
-      </Link>
+    <button
+      className="pill"
+      style={props.style}
+      onMouseEnter={moveChevron}
+      onMouseLeave={moveBackChevron}
+      onClick={handleOnClick}
+    >
+      {props.children}
+
       <img
         className="Chevron"
-        style={{ height: "75%", margin: "1px -5px 0px 10px" }}
+        id="ChevronIcon"
+        style={{ height: "75%", margin: "2px -5px 0px 10px" }}
         src={Chevron}
         alt="Chevron"
       />
